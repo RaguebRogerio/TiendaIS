d110aa238ec9c33f6e63a1f782b8ad21c7bfd1f0
RaguebRogerio <70453161+RaguebRogerio@users.noreply.github.com>
2022-01-20 11:45:49 -0300

    intento de implementar modal

--- src/Paginas/Productos/Productos.js
+++ src/Paginas/Productos/Productos.js
@@ -4,11 +4,12 @@ import Tabla from "../../Componentes/Tabla/Tabla"
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
 import Container from "../../Componentes/Container/Container"
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Button from '@mui/material/Button';
+import Modal from "../../Componentes/Modal/Modal";
 import { rootPath } from "../../App";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useState } from "react";
 const Productos = ()=>{
-    let history = useHistory();
     const headers =[
         {text:"Descripcion"},
         {text:"Precio"},
@@ -16,19 +17,22 @@ const Productos = ()=>{
         {text:"Eliminar"},
     ]
     const rows = [
-        {descripcion: 'Remera Puma', precio:'$ 200', editar: <EditIcon color="primary"/>, eliminar: <DeleteForeverIcon color="primary"/>},
-        {descripcion: 'Remera Puma', precio:'$ 200', editar: <EditIcon color="primary"/>, eliminar: <DeleteForeverIcon color="primary"/>},
-        {descripcion: 'Remera Puma', precio:'$ 200', editar: <EditIcon color="primary"/>, eliminar: <DeleteForeverIcon color="primary"/>},
-        {descripcion: 'Remera Puma', precio:'$ 200', editar: <EditIcon color="primary"/>, eliminar: <DeleteForeverIcon color="primary"/>},
-        {descripcion: 'Remera Puma', precio:'$ 200', editar: <EditIcon color="primary"/>, eliminar: <DeleteForeverIcon color="primary"/>},
+        {descripcion: 'Remera Puma1', precio:'$ 200', editar: <EditIcon color="primary"/>, eliminar: <DeleteForeverIcon color="primary"/>},
+        {descripcion: 'Remera Puma2', precio:'$ 200', editar: <EditIcon color="primary"/>, eliminar: <DeleteForeverIcon color="primary"/>},
+        {descripcion: 'Remera Puma3', precio:'$ 200', editar: <EditIcon color="primary"/>, eliminar: <DeleteForeverIcon color="primary"/>},
+        {descripcion: 'Remera Puma4', precio:'$ 200', editar: <EditIcon color="primary"/>, eliminar: <DeleteForeverIcon color="primary"/>},
+        {descripcion: 'Remera Puma5', precio:'$ 200', editar: <EditIcon color="primary"/>, eliminar: <DeleteForeverIcon color="primary"/>},
       ];
 
+    let history = useHistory();
     return(
         <Container>
-            <NavBar></NavBar>
-            <h1 style={{margin: "0px"}}>Productos</h1>
-            <Button variant="contained" onClick={()=>history.push(rootPath+'/nuevoproducto')} style = {{margin:"0px"}} size="large">Nuevo producto</Button>
-            <Tabla rows={rows} headers={headers}></Tabla>
+            <div>
+                <NavBar></NavBar>
+                <h1 style={{margin: "0px"}}>Productos</h1>
+                <Button variant="contained" onClick={()=>  history.push(rootPath+'/nuevoproducto') } style = {{margin:"0px"}} size="large">Nuevo producto</Button>
+                <Tabla rows={rows} headers={headers}></Tabla>
+            </div>
         </Container>
     )
 }
